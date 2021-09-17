58489194bca0d40779279eea44cf1d57333188ac
Kbow009 <bowman.kristin@gmail.com>
2021-09-17 16:49:38 -0700

    Configure JPA project, stub Dog

---
+++ JPADogs/src/main/java/com/skilldistillery/jpacrud/entities/Dog.java
@@ -0,0 +1,43 @@
+package com.skilldistillery.jpacrud.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Dog {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String breed;
+
+	public Dog() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Dog [id=" + id + ", breed=" + breed + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+
+}
