96c9f7ee41eb2cd0a855a2886e5caeded439a963
Kbow009 <bowman.kristin@gmail.com>
2021-09-17 17:04:44 -0700

    Create Boot MVC

---
+++ DoggyDogs/src/main/java/com/skilldistillery/jpacrud/DoggyDogsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggyDogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggyDogsApplication.class, args);
+	}
+
+}
